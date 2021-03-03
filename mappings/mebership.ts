4e3a8681898dbe0bbd0006e8241d4553d4fc2bcc
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-03-03 12:24:24 +0000

    updating imports in membership mapping

--- mappings/mebership.ts
+++ mappings/mebership.ts
@@ -1,2 +1,9 @@
 import BN from 'bn.js'
-import { Bytes } from '@polkadot/types'
+import { ApiPromise } from '@polkadot/api'
+import { Option } from '@polkadot/types/codec'
+import { Codec } from '@polkadot/types/types'
+import { Membership as Profile } from '@joystream/types/members'
+
+import { DB, getLogger } from '../../generated/hydra-processor'
+import { Block, Network } from '../../generated/graphql-server/src/modules/block/block.model'
+import { Membership } from '../../generated/graphql-server/src/modules/membership/membership.model'
